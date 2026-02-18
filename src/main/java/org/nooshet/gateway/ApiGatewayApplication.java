7a90912f7f2cb587c031e20e4c34a53ddd240967
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-02-18 10:55:51 +0400

    fix: exclude servlet auto-config to force webflux

--- src/main/java/org/nooshet/gateway/ApiGatewayApplication.java
+++ src/main/java/org/nooshet/gateway/ApiGatewayApplication.java
@@ -2,9 +2,14 @@ package org.nooshet.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    DispatcherServletAutoConfiguration.class,
+    ServletWebServerFactoryAutoConfiguration.class
+})
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
 
