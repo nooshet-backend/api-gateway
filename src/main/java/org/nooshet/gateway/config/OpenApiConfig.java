375fb54d83ffbddec151d9f079fa5ebe78b51308
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-02-18 10:28:42 +0400

    fixed charts

---
+++ src/main/java/org/nooshet/gateway/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package org.nooshet.gateway.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Gateway")
+                        .version("1.0.0")
+                        .description("API Gateway for Nooshet System"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", new io.swagger.v3.oas.models.security.SecurityScheme()
+                                .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Enter your JWT token")))
+                .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement().addList("bearerAuth"));
+    }
+}
